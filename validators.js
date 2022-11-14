66b9dc4c9d20b35cf78e08289cb329ec7074ef84
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-11-14 21:21:28 +0100

    second commit

--- validators.js
+++ validators.js
@@ -35,13 +35,15 @@ const userSchema = Joi.object({
   email: Joi.string().email().max(255).required(),
   firstname: Joi.string().max(255).required(),
   lastname: Joi.string().max(255).required(),
+  city: Joi.string().max(255).required(),
+  language: Joi.string().max(255).required(),
 });
 
 const validateUser = (req, res, next) => {
-  const { firstname, lastname, email } = req.body;
+  const { firstname, lastname, email, city, language } = req.body;
 
   const { error } = userSchema.validate(
-    { firstname, lastname, email },
+    { firstname, lastname, email, city, language },
     { abortEarly: false }
   );
 
